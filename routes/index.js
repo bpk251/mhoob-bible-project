d1c42306faef5d16bd3fde24cf97b25b424f0093
bpk251 <44997439+bpk251@users.noreply.github.com>
2020-09-07 14:58:49 +0700

    add bible

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/add-bible', function(req, res, next) {
+  res.render('add-bible/add-bible');
+});
+
 module.exports = router;
